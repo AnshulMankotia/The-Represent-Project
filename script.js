66468fea5663e72f3e2af3b49eecddae2d747e7a
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-01-12 16:17:49 +0530

    fixing bugs and add some sections

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ navIcon.addEventListener('click',() => {
     navIcon.classList.toggle('active')
     navLinks.classList.toggle('active')
 })
+
+
+// for back to top button
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll" , ()=>{
+    if(window.pageYOffset >500){
+        toTop.classList.add("active")
+    }else{
+     toTop.classList.remove("active")   
+    }
+})
