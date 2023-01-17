5de369e28bfb46980812cd0ed2f424c120cae482
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-01-17 21:00:27 +0530

    merge all sections

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-const navIcon=document.querySelector('.navIcon');
-const navIcon2=document.querySelector('.xmark');
-const navLinks=document.querySelector('.mobile-navs')
-navIcon.addEventListener('click',() => {
-    navIcon.classList.toggle('active')
-    navIcon2.classList.toggle('active')
-    navLinks.classList.toggle('active')
-});
+// const navIcon=document.querySelector('.navIcon');
+// const navIcon2=document.querySelector('.xmark');
+// const navLinks=document.querySelector('.mobile-navs')
+// navIcon.addEventListener('click',() => {
+//     navIcon.classList.toggle('active')
+//     navIcon2.classList.toggle('active')
+//     navLinks.classList.toggle('active')
+// });
 
 
 // for back to top button
@@ -24,7 +24,6 @@ window.addEventListener("scroll" , ()=>{
 // for main logo image
 const changeImg = document.querySelector("#mainlogo");
 const changeImg1 = document.querySelector("#mainlogo1");
-
 window.addEventListener("scroll" , ()=>{
     if(window.pageYOffset >250){
         changeImg.classList.add("active")
@@ -35,11 +34,30 @@ window.addEventListener("scroll" , ()=>{
     }
 });
 
-const hideNav = document.querySelector(".navMenu");
-window.addEventListener("scroll" , ()=>{
-    if(window.pageYOffset >500){
-        hideNav.classList.remove("active")
-    }else{
-     hideNav.classList.add("active")     
-    }
+
+// for mobile navigation
+const menuBar = document.querySelector("#menuBar")
+const openNav= document.querySelector('.navMenu')
+// const blurBg= document.querySelector('body')
+
+menuBar.addEventListener('click', ()=>{
+openNav.classList.toggle('active')
+// blurBg.classList.toggle('active')
+});
+
+const click = document.querySelector(".main-section")
+const closeNav= document.querySelector('.navMenu')
+
+click.addEventListener('click', ()=>{
+closeNav.classList.toggle('active')
+});
+
+
+
+// open items in mobnav
+const openItem = document.querySelector('.new')
+const openSlab = document.querySelector('.newSection')
+
+openItem.addEventListener('click',()=>{
+openSlab.classList.toggle('active')
 });
